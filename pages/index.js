956d012f2748583e084ab46660a6d27f0a864a5d
sammed-sankonatti <sammed.sankonatti45@gmail.com>
2021-06-23 00:31:02 +0530

    done with header component of chrome used icons from heroicons/react

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Avatar from "../components/Avatar";
+import {ViewGridIcon, MicroPhoneIcon} from '@heroicons/react/solid';
 
 export default function Home() {
   return (
@@ -10,7 +12,34 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1> LETS BUILD GOOGLE CHROME in NEXTJS</h1>
+      {/* <h1> LETS BUILD GOOGLE CHROME in NEXTJS</h1> */}
+
+      {/* Header */}
+      <header className="flex  w-full p-5 justify-between text-sm text-gray-700" >
+        <div className="flex space-x-4 items-center " >
+          <p className="link" > About</p>
+          <p className="link"> Store</p>
+        </div>
+
+        <div className="flex space-x-4 items-center" >
+          <p className="link"> Gmail</p>
+          <p className="link"> Images</p>
+          {/* Icon */}
+
+          <ViewGridIcon className="h-10 p-2 rounded-full hover:bg-gray-100 cursor-pointer "  />
+
+
+
+          {/* Avatar */}
+          <Avatar url="https://avatars.githubusercontent.com/u/82900434?v=4" />
+        </div>
+      </header>
+
+
+      {/* Body */}
+
+
+      {/* footer */}
     </div>
   )
 }
