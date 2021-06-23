4f92b76f52f0e76d99c7200d6e1215bc1e2b25df
sammed-sankonatti <sammed.sankonatti45@gmail.com>
2021-06-23 18:56:22 +0530

    everything done except the API is working correctly

---
+++ keys.js
@@ -0,0 +1,7 @@
+// API KEY
+
+export const API_KEY = "AIzaSyAr0YYWt4ddWI1CvBTtGjoEsZFrdTMcpVI";
+
+// context key
+
+export const CONTEXT_KEY = "a21080e56c155c658";
--- pages/search.js
+++ pages/search.js
@@ -1,18 +1,46 @@
 import Head from 'next/head'
 import Header from './../components/Header';
+import { API_KEY , CONTEXT_KEY   } from "../keys";
+import Response from "../Response";
+import { useRouter } from 'next/router';
+import SearchResults from './../components/SearchResults';
 
-function Search() {
+function Search( {results} ) {
+
+    const router = useRouter();
+
+    console.log(results);
+
     return (
         <div>
             <Head>
-                <title> search result </title>
+                <title> {router.query.term} - google search</title>
             </Head>
 
-            {/* Header */}
             <Header />
+
             {/* search results */}
+
+            <SearchResults results={results} />
+
         </div>
     )
 }
 
 export default Search;
+
+export async  function getServerSideProps(context){
+    const useDummyData = false;
+    const startIndex =  context.query.start || "0";
+
+    const data = useDummyData ? Response :  await fetch(
+        `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.term}&start=${startIndex}`).then(response => response.json());
+
+    // after server side rendering ,  pass the result to client
+
+    return {
+        props: {
+            results : data,
+        },
+    }
+}
