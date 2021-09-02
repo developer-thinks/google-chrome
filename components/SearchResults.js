2517f88c355f64e302534dce876395e816c8e1f5
sammed-sankonatti <sammed.sankonatti45@gmail.com>
2021-09-03 04:18:27 +0530

    photos added

--- components/SearchResults.js
+++ components/SearchResults.js
@@ -6,20 +6,63 @@ function SearchResults({results}) {
     return (
         <div className="mx-auto w-full px-3 sm:pl-[5%] md: pl-[14%] lg:pl-52 " >
             <p className="text-gray-600 text-md mb-5 mt-3" > About {results.searchInformation?.formattedTotalResults} Results ( {results.searchInformation?.formattedSearchTime} seconds)  </p>
-            <Image 
-            src="https://www.linkpicture.com/q/IMG_20210629_105415.jpg"
-            width={250}
-            height={280}
+          
+        <Image 
+            src="https://www.linkpicture.com/q/IMG_20210626_125723-1.jpg"
+            width={200}
+            height={240}
+            className="cursor-pointer  "
+            
+        />
+        <Image 
+            src="https://www.linkpicture.com/q/IMG_20210629_105446-1.jpg"
+            width={200}
+            height={240}
+            className="cursor-pointer  "
+            
+        />
+        <Image 
+            src="https://www.linkpicture.com/q/IMG_20210626_125728-1.jpg"
+            width={200}
+            height={240}
             className="cursor-pointer  "
             
         />
         {/* <Image 
-            src="https://www.linkpicture.com/q/IMG_20210629_105415.jpg"
+            src="https://www.linkpicture.com/q/IMG_20210628_110135-1.jpg"
             width={250}
-            height={280}
+            height={300}
             className="cursor-pointer  "
             
         /> */}
+        {/* <Image 
+            src="https://www.linkpicture.com/q/IMG_20210101_052223-1.jpg"
+            width={250}
+            height={300}
+            className="cursor-pointer "    
+        />
+
+          <Image 
+            src="https://www.linkpicture.com/q/IMG_20210619_122340-1.jpg"
+            width={250}
+            height={300}
+            className="cursor-pointer  "
+            
+        />
+            <Image 
+            src="https://www.linkpicture.com/q/IMG_20210629_105415.jpg"
+            width={250}
+            height={300}
+            className="cursor-pointer  "
+            
+        /> */}
+        {/* <Image 
+            src="https://www.linkpicture.com/q/IMG_20210621_122744-1.jpg"
+            width={240}
+            height={300}
+            className="cursor-pointer  "
+        /> */}
+
             {results.items?.map((result)=>(
                     <div key={result.link} className="max-w-xl  mb-8" >
                         <div className="group" >
